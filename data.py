7fe70bd0858f093f28a2866325835b9bec903a1f
Adrien2320 <mertensadrien23@hotmail.be>
2023-01-04 13:19:04 +0100

    version 2.0  ajout de la class VaultItem

--- data.py
+++ data.py
@@ -1,5 +1,23 @@
 class VaultItem:
-    pass
+    """Class data Vault"""
+
+    def __init__(self, name: str, login: str, password: str) -> None:
+        """
+        Constructor of VaultItem
+        :param name:
+        :param login:
+        :param password:
+        """
+        self.name = name
+        self.login = login
+        self.password = password
+
+    def __str__(self) -> str:
+        """
+        name the object
+        :return: string
+        """
+        return self.name
 
 
 class User:
